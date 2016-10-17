29f6905a8e178126a8f3eca57850006e6567b5ad
GeroBlacksmith <satanica1>
2016-10-17 01:30:30 -0300

    Imagenes de clima y mejora del perfil

--- perfil.php
+++ perfil.php
@@ -57,28 +57,46 @@ and open the template in the editor.
             <div class="card-panel">
                 Usuario: <?= $nombre ?><br>
                 Zonas elegidas:<br>
-                Ninguna
-
-            </div>
-        </div>
-        <div class="col s4">
-            <div class="card-panel">
-                <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
-                    <a class="btn-floating btn-large red" href="agregar-zona.php">
-                        <i class="large material-icons">add</i>
-                    </a>
-                </div>
-                Alertas zonas
-            </div>
-        </div>
-        <div class="col s4">
-            <div class="card-panel">
-                opciones
+                Agregar una zona para monitorear:<br>
                 <!-- carga dinamica de las zonas -->
                 <select class="browser-default">
                     <option>Neuquen</option>
                     <option>Allen</option>
                 </select>
+                <a class="waves-effect waves-light btn green darken-1"><i class="material-icons right">add</i>Agregar zona</a>
+            </div>
+        </div>
+        <div class="col s4">
+            <div class="card-panel">
+                <div>
+                    webservice de alertas<br>
+                    <img src="img/weather_rain.png" alt="imagen que representa el clima actual">
+
+                </div>
+                <ul class="collection with-header">
+                    <li class="collection-header"><h4>Alertas recientes</h4></li>
+                    <li class="collection-item">Alvin</li>
+                    <li class="collection-item">Alvin</li>
+                    <li class="collection-item">Alvin</li>
+                    <li class="collection-item">Alvin</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col s4">
+            <div class="card-panel">
+
+                <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+                    <a class="btn-floating btn-large red" alt="agregar contribucion" href="#">
+                        <i class="large material-icons">add</i>
+                    </a>
+                </div>
+                <ul class="collection with-header">
+                    <li class="collection-header"><h4>Contribuciones</h4></li>
+                    <li class="collection-item">Alvin</li>
+                    <li class="collection-item">Alvin</li>
+                    <li class="collection-item">Alvin</li>
+                    <li class="collection-item">Alvin</li>
+                </ul>
             </div>
         </div>
     </div>
